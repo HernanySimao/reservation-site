0e0fe0cf9b8d20b8f03b438a9cc109bec5792810
Hernany Simao <hernanysimao123@gmail.com>
2025-01-16 04:30:13 +0100

    ajustei o logo

--- components/Menu.vue
+++ components/Menu.vue
@@ -5,7 +5,9 @@
   <section>
     <div>
       <nav class="navbar bg-body-tertiary p-3">
-        <div class="container">
+        <div
+          class="container d-flex justify-content-between align-items-center"
+        >
           <a class="navbar-brand primary-theme-font">
             <img class="logo" src="/icons/logo.png" alt="" />
           </a>
@@ -24,9 +26,20 @@
 <style lang="sass" scoped>
 .navbar
   box-shadow: rgba(99, 99, 99, 0.2) 0px 2px 8px 0px
+
 .logo
   width: 10%
   height: auto
+
 .link
   text-decoration: none
+
+@media (max-width: 767px)  // Mobile
+  .logo
+    width: 50px
+    // display: none
+  .navbar .container
+    display: flex
+    justify-content: space-between
+    align-items: center
 </style>
